6f2f92a41dd68d4ce2a9e664d8de1720c43cf25a
AvavSam <avav2608005@gmail.com>
2025-01-21 02:31:41 +0800

    feat(home): slicing about section

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,9 +1,11 @@
+import AboutSection from "./sections/AboutSection";
 import HeroSection from "./sections/HeroSection";
 
 export default function HomePage() {
   return (
     <div>
       <HeroSection />
+      <AboutSection />
     </div>
   );
 }
