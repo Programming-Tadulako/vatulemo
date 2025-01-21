6167f70bb8d7d3f389546b86a247ea1c13a332fa
adzaky <muhadlim17@gmail.com>
2025-01-21 18:02:44 +0800

    chore(home): add id to sections

--- src/features/home/sections/HeroSection.tsx
+++ src/features/home/sections/HeroSection.tsx
@@ -3,7 +3,10 @@ import { FaDiscord, FaInstagram } from "react-icons/fa";
 
 export default function HeroSection() {
   return (
-    <section className="min-h-screen bg-[url('/assets/images/hero/bg_hero-waves.svg')] bg-cover bg-center bg-no-repeat p-5">
+    <section
+      id="hero"
+      className="min-h-screen bg-[url('/assets/images/hero/bg_hero-waves.svg')] bg-cover bg-center bg-no-repeat p-5"
+    >
       <div className="mx-auto flex max-w-screen-md flex-col gap-4 pt-32 text-white lg:items-center">
         <Image src="/assets/icons/ic_pt_full-white.svg" alt="Programming Tadulako" width={500} height={150} />
         <p className="text-lg lg:text-center lg:text-2xl">
