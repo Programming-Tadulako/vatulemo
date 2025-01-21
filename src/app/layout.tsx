0cf1a80d42663be4bbdac8cdbce2978d1e518a59
adzaky <muhadlim17@gmail.com>
2025-01-21 18:16:42 +0800

    chore(layout): remove unused hydration warning supression

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body className={`${poppins.className} antialiased`}>
         <WrapperLayout>{children}</WrapperLayout>
       </body>
