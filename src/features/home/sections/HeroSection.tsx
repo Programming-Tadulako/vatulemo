bfb3d975c7671ebb5e02ef09267d18fdd6f8ba41
adzaky <muhadlim17@gmail.com>
2025-01-21 12:59:43 +0800

    fix(home): fix hero section logo filename derp

--- src/features/home/sections/HeroSection.tsx
+++ src/features/home/sections/HeroSection.tsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
   return (
     <section className="min-h-screen bg-[url('/assets/images/hero/bg_hero-waves.svg')] bg-cover bg-center bg-no-repeat p-5">
       <div className="mx-auto flex max-w-screen-md flex-col gap-4 pt-32 lg:items-center">
-        <Image src="/assets/icons/ic_pt_long-dark.svg" alt="Programming Tadulako" width={500} height={150} />
+        <Image src="/assets/icons/ic_pt_full-white.svg" alt="Programming Tadulako" width={500} height={150} />
         <p className="text-lg lg:text-center lg:text-2xl">
           A community based in Palu, Indonesia. <br /> Learn about programming, softskills, and teamwork.
         </p>
