ead3b370f75ab079aac88d4cf5b273a5ddd95539
adzaky <muhadlim17@gmail.com>
2025-01-26 15:13:13 +0800

    remove(tailwindconfig): unused dark mode config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "tailwindcss";
 import tailwindCSSAnimate from "tailwindcss-animate";
 
 export default {
-  darkMode: ["class"],
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
