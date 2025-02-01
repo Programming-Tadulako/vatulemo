d549057c021a552b42c41b36bec6dcf7963df3e2
adzaky <muhadlim17@gmail.com>
2025-02-01 15:05:13 +0800

    style(comp/timeline): removing unnecessary padding and refining spacing

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -58,16 +58,14 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
 
   return (
     <div className="container bg-white" ref={containerRef}>
-      <div className="pt-20">
-        <h2 className="text-3xl font-bold text-pt-primary lg:text-4xl">Batch</h2>
-        <p className="max-w-sm text-sm text-neutral-700 md:text-base">
-          Sampai saat ini Programming Tadulako telah memiliki beberapa batch yaitu:
-        </p>
-      </div>
+      <h2 className="text-3xl font-bold text-pt-primary lg:text-4xl">Batch</h2>
+      <p className="max-w-sm text-sm text-neutral-700 md:text-base">
+        Sampai saat ini Programming Tadulako telah memiliki beberapa batch yaitu:
+      </p>
 
       <div ref={ref} className="relative mx-auto pb-20">
         {data.map((item, index) => (
-          <div key={index} className="flex justify-start pt-12 md:gap-10 md:pt-40">
+          <div key={index} className="flex justify-start pt-12 md:gap-10 md:pt-32">
             <div className="sticky top-40 z-40 flex max-w-xs flex-col items-center self-start md:w-full md:flex-row lg:max-w-sm">
               <div className="absolute left-3 flex h-10 w-10 items-center justify-center rounded-full bg-white md:left-3">
                 <div className="h-4 w-4 rounded-full border border-neutral-300 bg-neutral-200 p-2" />
