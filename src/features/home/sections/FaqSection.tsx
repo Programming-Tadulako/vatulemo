fd4e381a283c147ec809ecf05efc1187c4bfeb73
ifalfahri <ifaltampan69@gmail.com>
2025-02-01 23:53:37 +0800

    style(home/faq): wrap FAQ section in a container and adjust padding

--- src/features/home/sections/FaqSection.tsx
+++ src/features/home/sections/FaqSection.tsx
@@ -4,15 +4,17 @@ import { FAQ_DATA } from "@/features/home/constants";
 export default function BatchSection() {
   return (
     <section id="faq">
-      <h1 className="pb-8 pt-2 text-center text-4xl font-bold uppercase text-pt-primary">FAQ</h1>
-      <Accordion type="single" collapsible className="container">
-        {FAQ_DATA.map((faq, index) => (
-          <AccordionItem key={index} value={`item-${index + 1}`}>
-            <AccordionTrigger>{faq.title}</AccordionTrigger>
-            <AccordionContent>{faq.description}</AccordionContent>
-          </AccordionItem>
-        ))}
-      </Accordion>
+      <div className="container py-20">
+        <h1 className="pb-8 text-center text-4xl font-bold uppercase text-pt-primary">FAQ</h1>
+        <Accordion type="single" collapsible>
+          {FAQ_DATA.map((faq, index) => (
+            <AccordionItem key={index} value={`item-${index + 1}`}>
+              <AccordionTrigger>{faq.title}</AccordionTrigger>
+              <AccordionContent>{faq.description}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </section>
   );
 }
