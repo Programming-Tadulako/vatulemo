efffcd04eb3377e1cdfef02005a029b2b8bcbf70
ifalfahri <ifaltampan69@gmail.com>
2025-02-13 12:55:35 +0800

    feat(home/batch): wrap Batch Section in SlideUp animation component

--- src/features/home/sections/BatchSection.tsx
+++ src/features/home/sections/BatchSection.tsx
@@ -1,10 +1,13 @@
+import { SlideUp } from "@/components/ui/animations/slide-up";
 import { Timeline } from "@/components/ui/timeline";
 import { BATCH_DATA } from "@/features/home/constants";
 
 export default function BatchSection() {
   return (
     <section id="batch">
-      <Timeline data={BATCH_DATA} />
+      <SlideUp>
+        <Timeline data={BATCH_DATA} />
+      </SlideUp>
     </section>
   );
 }
