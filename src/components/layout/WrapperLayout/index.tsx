6f644ee9f6df535307f44027037f78038769d1fa
adzaky <muhadlim17@gmail.com>
2026-07-04 16:17:05 +0800

    feat: integrate lenis smooth scrolling

--- src/components/layout/WrapperLayout/index.tsx
+++ src/components/layout/WrapperLayout/index.tsx
@@ -1,12 +1,23 @@
-import { Navbar } from "../Navbar";
-import { Footer } from "../Footer";
+import { ReactLenis } from "lenis/react";
+import { Footer } from "@/components/layout/Footer";
+import { Navbar } from "@/components/layout/Navbar";
 
 export default function WrapperLayout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <ReactLenis
+      root
+      options={{
+        autoRaf: true,
+        autoToggle: true,
+        anchors: true,
+        stopInertiaOnNavigate: true,
+        allowNestedScroll: false,
+        naiveDimensions: false,
+      }}
+    >
       <Navbar />
       {children}
       <Footer />
-    </>
+    </ReactLenis>
   );
 }
